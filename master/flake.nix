a8214344aa74f6f88afcdb1a830b34ec52f43127
hugosenari <hugosenari@gmail.com>
2022-03-12 03:34:59 -0300

    chore: re index mort

---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A dead code locator for Nim'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-mort-master.flake = false;
+  inputs.src-mort-master.owner = "jyapayne";
+  inputs.src-mort-master.ref   = "refs/heads/master";
+  inputs.src-mort-master.repo  = "mort";
+  inputs.src-mort-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-mort-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-mort-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
